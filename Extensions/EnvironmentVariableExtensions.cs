3ed75dba102089e61fa849e043710dca3590e243
Mert <mert172317@gmail.com>
2022-03-13 21:46:23 +0300

    ConnectionString duzenlendi.

---
+++ Extensions/EnvironmentVariableExtensions.cs
@@ -0,0 +1,24 @@
+using System;
+using Microsoft.Extensions.Configuration;
+
+namespace Extensions
+{
+    public static class EnvironmentVariableExtensions
+    {
+        public static string GetConnectionStringFromEnvironment(this IConfiguration configuration, string name = null)
+        {
+            if(string.IsNullOrEmpty(name))
+            {
+                string envServer = Environment.GetEnvironmentVariable("DB_HOST"); 
+                string envDatabase = Environment.GetEnvironmentVariable("DB_NAME"); 
+                string envUser = Environment.GetEnvironmentVariable("DB_USER"); 
+                string envPassword = Environment.GetEnvironmentVariable("DB_PASS"); 
+                return "Server="+envServer+";Database="+envDatabase+";Uid="+envUser+";Pwd="+envPassword+";";
+            }
+            else
+            {
+                return Environment.GetEnvironmentVariable(name);
+            }
+        }
+    }
+    
